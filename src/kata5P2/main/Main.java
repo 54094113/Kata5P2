04c414e940e36bcfdb49b2b0bccda597040a8043
Entrar <Entrar@APD-L13>
2017-12-01 09:10:34 +0000

    Kata5P2 v1

--- src/kata5P2/main/Main.java
+++ src/kata5P2/main/Main.java
@@ -1,18 +1,18 @@
-package kata4.main;
+package kata5P2.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import kata4.model.Histogram;
-import kata4.model.Mail;
-import kata4.view.HistogramDisplay;
-import kata4.view.MailHistogramBuilder;
-import kata4.view.MailListReader;
+import kata5P2.model.Histogram;
+import kata5P2.model.Mail;
+import kata5P2.view.HistogramDisplay;
+import kata5P2.view.MailHistogramBuilder;
+import kata5P2.view.MailListReader;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String filename = "C:\\Users\\usuario\\Downloads\\emails.txt";
+        String filename = "C:\\Users\\Entrar\\Desktop\\emails.txt";
         List<Mail> mailList = MailListReader.read(filename);
         Histogram<String> histogram = MailHistogramBuilder.build(mailList);
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
